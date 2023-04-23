66c9874eab5e3d6b02889bea69c7e57cc13a3748
sahilwagh03 <sahilwagh142@gmail.com>
2023-04-23 18:57:52 +0530

    effect updated

--- src/components/FadeComponent/Fadecomponent.jsx
+++ src/components/FadeComponent/Fadecomponent.jsx
@@ -2,24 +2,25 @@ import React, { useRef, useEffect, useState } from 'react';
 
 function Fadecomponent(props) {
   const ref = useRef(null);
-  const [visible, setVisible] = useState(false);
 
   useEffect(() => {
     const onScroll = () => {
-      if (ref.current && !visible) {
+      if (ref.current) {
         const top = ref.current.getBoundingClientRect().top;
         const windowHeight = window.innerHeight;
         if (top < windowHeight * 0.75) {
-          setVisible(true);
+          ref.current.classList.add('visible');
+        } else {
+          ref.current.classList.remove('visible');
         }
       }
     };
     window.addEventListener('scroll', onScroll);
     return () => window.removeEventListener('scroll', onScroll);
-  }, [visible]);
+  }, []);
 
   return (
-    <div ref={ref} className={`fade-in${visible ? ' visible' : ''}`}>
+    <div ref={ref} className={`fade-in`}>
       {props.children}
     </div>
   );
