e4dd2b82f11920e4fb15932cbf8bae96267d6022
sahilwagh03 <sahilwagh142@gmail.com>
2023-07-08 18:38:22 +0530

    fix

--- src/components/FadeComponent/Fadecomponent.jsx
+++ src/components/FadeComponent/Fadecomponent.jsx
@@ -1,29 +1,28 @@
-import React, { useRef, useEffect, useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function Fadecomponent(props) {
   const ref = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const onScroll = () => {
-      if (ref.current) {
+      if (!isVisible && ref.current) {
         const top = ref.current.getBoundingClientRect().top;
         const windowHeight = window.innerHeight;
         if (top < windowHeight * 0.75) {
-          ref.current.classList.add('visible');
-        } else {
-          ref.current.classList.remove('visible');
+          setIsVisible(true);
         }
       }
     };
     window.addEventListener('scroll', onScroll);
     return () => window.removeEventListener('scroll', onScroll);
-  }, []);
+  }, [isVisible]);
 
   return (
-    <div ref={ref} className={`fade-in`}>
+    <div ref={ref} className={`fade-in ${isVisible ? 'visible' : ''}`}>
       {props.children}
     </div>
   );
 }
-export default Fadecomponent;
 
+export default Fadecomponent;
